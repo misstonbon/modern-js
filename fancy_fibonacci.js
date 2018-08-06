c5bcc747f6cc6ef12101d8951d553184dd3dcd6d
misstonbon <plusdeltah@gmail.com>
2018-08-06 14:25:13 -0700

    adds recursive fibonacci

--- fancy_fibonacci.js
+++ fancy_fibonacci.js
@@ -18,13 +18,28 @@ function simpleBenchmark(fn, name, ...args) {
   const diff = process.hrtime(start);
   console.log(`${name} function took ${(diff[0] * NS_PER_SEC + diff[1]) *
     MS_PER_NS}ms with result:`);
-  console.log(result);
+//   console.log(result);
 }
 
 // Tests for charFreq function.
 console.log('Start fancy fibonacci test:\n');
-simpleBenchmark(fibonacci, "Fibonacci", 8);
-simpleBenchmark(fibonacci, "Fibonacci", 20);
-simpleBenchmark(fibonacci, "Fibonacci", 30);
-simpleBenchmark(fibonacci, "Fibonacci", 40);
-simpleBenchmark(fibonacci, "Fibonacci", 50);
+simpleBenchmark(fibonacci, "Fibonacci 10", 10);
+simpleBenchmark(fibonacci, "Fibonacci 200", 200);
+simpleBenchmark(fibonacci, "Fibonacci 300", 300);
+simpleBenchmark(fibonacci, "Fibonacci 400", 400);
+simpleBenchmark(fibonacci, "Fibonacci 15000", 15000);
+
+function recursive_fib(n) {
+        if (n < 2){
+          return n
+        }
+        return recursive_fib(n - 1) + recursive_fib(n - 2)
+}
+
+console.log('Start recursive fibonacci test:\n');
+simpleBenchmark(fibonacci, "Recursive Fibonacci 10", 10);
+simpleBenchmark(fibonacci, "Recursive Fibonacci 200", 200);
+simpleBenchmark(fibonacci, "Recursive Fibonacci 300", 300);
+simpleBenchmark(fibonacci, "Recursive Fibonacci 400 ", 400);
+simpleBenchmark(fibonacci, "Recursive Fibonacci 15000 ", 15000);
+
